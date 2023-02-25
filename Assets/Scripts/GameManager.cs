dd3c53561f4a488b55fe6446233ae3db1827bc2c
Antía Roig Regueira <antiaroig@gmail.com>
2023-02-25 19:35:01 +0100

    Xeralización RequestHandler para JSON e imaxe

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,14 +1,32 @@
-using System.Collections;
-using System.Collections.Generic;
+using System;
 using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
+
+    private const string API_URI = "https://servizos.meteogalicia.gal/mgrss/observacion/jsonCamaras.action";
+
+    [SerializeField] private RawImage image;
+
+    private CameraList cameraList;
     
     void Start() {
-        StartCoroutine(RequestHandler.GetRequest("https://servizos.meteogalicia.gal/mgrss/observacion/jsonCamaras.action"));
+        loadNewImage();
     }
 
     void Update() {
         
+    }
+
+    private void loadNewImage() {
+        StartCoroutine(RequestHandler.GetRequest(API_URI, false, handler => {
+            cameraList = JsonUtility.FromJson<CameraList>(handler.text);
+            Debug.Log(DateTime.Parse(cameraList.listaCamaras[0].dataUltimaAct));
+            
+            StartCoroutine(RequestHandler.GetRequest(cameraList.listaCamaras[0].imaxeCamara, true, handler => {
+                image.texture = ((DownloadHandlerTexture)handler).texture;
+            }));
+        }));
     }
 }
